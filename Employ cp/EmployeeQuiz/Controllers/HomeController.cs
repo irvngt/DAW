b19c0fd93e2636ce583034283c140bbe417bc073
irving <comet1990@hotmail.com>
2014-07-30 02:00:15 -0500

    more

---
+++ Employ cp/EmployeeQuiz/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeQuiz.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index(Employe emp)
+        {
+
+            //Creo el objeto del modelo de datos
+            Payrolldm nomina = new Payrolldm(
+                @"C:\Users\Irving\Documents\Visual Studio 2013\Projects\Employ cp\EmployeeQuiz\Models\EmployeeDb.csv");
+            
+            //busco el empleado dado su id
+            var empleado = nomina.GetEmployeById(emp.Id);
+
+            if (empleado == null)
+                return View("Error");
+
+            return View("WorkerView", empleado);
+        }
+
+
+
+
+    }
+}
